ee95bab4a6713f741cb1bdd44d0af01a965eb85b
x3mboy <ealucena@falabella.cl>
2021-04-28 14:13:33 -0400

    Agregando los archivos css , js y el html de peru

---
+++ script.js
@@ -0,0 +1,58 @@
+let name = prompt ("Ingresa tu nombre");
+
+const checkBtn = (option) => {
+  switch(option) {
+
+    case "a.Piura":
+    alert ("Respuesta incorrecta")
+    break;
+
+    case "b.Cajamarca":
+    alert ("Respuesta incorrecta")
+    break;
+
+    case "c.Cusco":
+    alert ("Respuesta correcta")
+    break;
+
+
+  }
+}
+
+const Pachacamac = (option) => {
+  switch(option) {
+
+    case "a.Jesus Maria":
+    alert ("Respuesta incorrecta")
+    break;
+
+    case "b.Lurín":
+    alert ("Respuesta correcta")
+    break;
+
+    case "c.Villa el salvador":
+    alert ("Respuesta incorrecta")
+    break;
+
+
+  }
+}
+
+const Ollantaytambo = (option) => {
+  switch(option) {
+
+    case "a.Ucayali":
+    alert ("Respuesta incorrecta")
+    break;
+
+    case "b.Cusco":
+    alert ("Respuesta correcta")
+    break;
+
+    case "c.Madre de Dios":
+    alert ("Respuesta incorrecta")
+    break;
+
+
+  }
+}
