689b259306bd83b009a33b397551e5ab65693c63
cuandotocatoca <83358705+cuandotocatoca@users.noreply.github.com>
2021-04-28 22:01:19 -0500

    2021-04-28
    Arreglos que hice hoy que estaba trabajando con tica

---
+++ script-vz.js
@@ -0,0 +1,74 @@
+let name = prompt ("Ingresa tu nombre");
+
+function question1 (type) {
+  const  formucontainer = document.querySelector("#formu");
+  const formucontainer1a = document.querySelector("#form1");
+  const formucontainer2a = document.querySelector("#form2")
+  formucontainer.style.display = "none";
+  formucontainer1a.style.display = "block";
+  switch (type) {
+    case "Peru":
+      const form1a = document.querySelector("#form1");
+      form1a.style.display = "block";
+      console.log("Peru")
+      break;
+    case "Venezuela":
+      const form2b = document.querySelector("#form2");
+      form2b.style.display = "block";
+      console.log ("Venezuela")
+      break;
+    default:
+      break;
+  }
+}
+
+const checkBtn = (option) => {
+  switch(option) {
+
+    case "a.Caracas":
+      alert ("Respuesta correcta")
+      break;
+
+    case "b.Tucupita":
+      alert ("Respuesta incorrecta")
+      break;
+
+    case "c.San Cristobal":
+      alert ("Respuesta incorrecta")
+      break;
+  }
+}
+
+const CuevaDelGuacharo = (option) => {
+  switch(option) {
+
+    case "a.Trujillo":
+      alert ("Respuesta incorrecta")
+      break;
+
+    case "b.Barinas":
+      alert ("Respuesta incorrecta")
+      break;
+
+    case "c.Monagas":
+      alert ("Respuesta correcta")
+      break;
+  }
+}
+
+const MonumentoMantoDeMaria = (option) => {
+  switch(option) {
+
+    case "a.Bolivar":
+      alert ("Respuesta incorrecta")
+      break;
+
+    case "b.Lara":
+      alert ("Respuesta correcta")
+      break;
+
+    case "c.Delta Amacuro":
+      alert ("Respuesta incorrecta")
+      break;
+  }
+}
